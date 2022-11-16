93c7d95c7fa5bf137c81ffd4d95e31573c0e8b3e
Sergei Klapushev <klapischeff@yandex.ru>
2022-11-16 19:29:46 +0300

    Task2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30 */
+
+Messager("Введите число М: ");
+int M = InputNum();
+Messager("Введите число N: ");
+int N = InputNum();
+int sum = 0;
+
+Messager($"M = {M}; N = {N} -> ");
+
+OutputOfEvenNaturalNumbers(M, N, sum);
+
+
+void Messager(string message)
+{
+    Console.Write(message);
+}
+
+int InputNum()
+{
+    return Convert.ToInt32(Console.ReadLine()); ;
+}
+
+void OutputOfEvenNaturalNumbers(int m, int n, int s)
+{
+    if (m > n)
+    {
+        Console.Write(s);
+        return;
+    }
+    if (m <= n)
+    {
+        s = s + m;
+    }
+    OutputOfEvenNaturalNumbers(m + 1, n, s);
+
+}
