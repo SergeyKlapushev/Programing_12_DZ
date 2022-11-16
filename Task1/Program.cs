0edf3ff4c53bcfdad2f03bfe9c6f3155de925142
Sergei Klapushev <klapischeff@yandex.ru>
2022-11-16 19:01:57 +0300

    Task1 comleted

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8" */
+
+Messager("Введите число М: ");
+int M = InputNum();
+Messager("Введите число N: ");
+int N = InputNum();
+
+OutputOfEvenNaturalNumbers(M, N);
+
+void Messager(string message)
+{
+    Console.Write(message);
+}
+
+int InputNum()
+{
+    return Convert.ToInt32(Console.ReadLine()); ;
+}
+
+void OutputOfEvenNaturalNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+    if(m%2 == 0)
+    {
+        Console.Write($"{m} ");
+    }
+    
+
+
+    OutputOfEvenNaturalNumbers(m + 1, n);
+
+}
